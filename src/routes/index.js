41fe49de00c3e0147769bfed4bdfd55fccbfeba4
thomasHeitFux <thomas41392@gmail.com>
2022-05-02 19:04:32 -0300

    [ trying to change of img] src/routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,7 +27,7 @@ const getApiInfo = async () => {
                 defense: pokemon.data.stats[2].base_stat,
                 speed: pokemon.data.stats[5].base_stat,
                 type: pokemon.data.types.map(p => p.type.name),
-                img: pokemon.data.sprites.front_transparent
+                img: pokemon.data.sprites.front_shiny
             }
         }))
     return result
